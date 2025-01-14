62e2dadf697d4b59abe5856198b16c6c8fb8ffcf
Rusith Tharindu Athukorala <103258178+rusith-tharindu@users.noreply.github.com>
2025-01-14 20:44:20 +0000

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/e-plantShopping/",  // Correct base path for GitHub Pages deployment
+  base: '/e-plantShopping/',  // Ensure this is set to the base path where the app is hosted
   plugins: [react()],
 })
